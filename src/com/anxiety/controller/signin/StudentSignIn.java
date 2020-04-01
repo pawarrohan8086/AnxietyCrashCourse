e1ad48f62ad90968cba612a79849fefe13ae5785
pawarrohan8086 <pawarrohan8086@gmail.com>
2020-04-01 22:14:49 +0530

    requestDispatcher with url rewritting

--- src/com/anxiety/controller/signin/StudentSignIn.java
+++ src/com/anxiety/controller/signin/StudentSignIn.java
@@ -1,34 +1,35 @@
 package com.anxiety.controller.signin;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-@WebServlet("/StudentSignIn")
+@WebServlet(name = "StudentSignIn", urlPatterns = {"/StudentSignIn"})
 public class StudentSignIn extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		
+		 HttpSession session=request.getSession(true);
+		 session.setAttribute("uname", request.getParameter("uname"));
+		 session.setAttribute("email", request.getParameter("email"));
+		 session.setAttribute("pswd", request.getParameter("pswd"));
+		 session.setAttribute("ipaddress",request.getRemoteAddr());
+		 session.setAttribute("hostname", request.getRemoteHost());
+		 session.setAttribute("date",new Date().toString());
 
-		
-		HttpSession session=request.getSession(false);
-		String email=(String) session.getAttribute("email");
-		String password=(String)session.getAttribute("password");
-		String ipaddress=(String)session.getAttribute("ipaddress");
-		String hostname=(String)session.getAttribute("hostname");
-		String location=(String)session.getAttribute("location");
-		String date=(String)session.getAttribute("date");
-		
+		//store into database
+		 request.setAttribute("flag","needs-validation");
 		RequestDispatcher rd=request.getRequestDispatcher(response.encodeURL("StudentEnroll.jsp"));
 		rd.include(request, response);
 		
